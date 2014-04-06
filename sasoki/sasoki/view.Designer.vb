1065e32cf95a011bd2065b156ba8d870b9bbc9ac
ALi <ali.prog@yahoo.com>
2014-04-06 22:13:46 +0300

    all ok.

--- sasoki/sasoki/view.Designer.vb
+++ sasoki/sasoki/view.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class view
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,29 +22,37 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
+        'MonthCalendar1
+        '
+        Me.MonthCalendar1.Location = New System.Drawing.Point(0, 0)
+        Me.MonthCalendar1.Name = "MonthCalendar1"
+        Me.MonthCalendar1.TabIndex = 0
+        '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(197, 226)
+        Me.Button1.Location = New System.Drawing.Point(244, 174)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
+        Me.Button1.TabIndex = 1
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'Form1
+        'view
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(331, 209)
         Me.Controls.Add(Me.Button1)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.MonthCalendar1)
+        Me.Name = "view"
+        Me.Text = "view"
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents MonthCalendar1 As System.Windows.Forms.MonthCalendar
     Friend WithEvents Button1 As System.Windows.Forms.Button
-
 End Class
---
+++ sasoki/sasoki/view.vb
@@ -0,0 +1,8 @@
+﻿Public Class view
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'هذا الامر يغلق الفووورم ..
+        Close()
+
+    End Sub
+End Class
